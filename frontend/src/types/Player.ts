e3d114a9bc3ff2a8dc40d832711e1ce2df79bfe5
Cody Breaker <152615629+CodyBreaker@users.noreply.github.com>
2025-07-17 15:20:30 +0200

    Added color option to the player

--- frontend/src/types/Player.ts
+++ frontend/src/types/Player.ts
@@ -3,6 +3,7 @@ import type { DieAction } from "./DieAction";
 export type Player = {
     id: number;
     name: string;
+    color: string | null;
     is_ready: boolean;
     home_base: number;
     move_die?: DieAction;
