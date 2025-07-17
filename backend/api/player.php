2d2d934f9da4f65a1031279b3fd4394bccb8d734
Cody Breaker <152615629+CodyBreaker@users.noreply.github.com>
2025-07-17 18:22:18 +0200

    Fixed CORS Api issue

--- backend/api/player.php
+++ backend/api/player.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
@@ -8,6 +8,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
 }
+
+header('Content-Type: application/json');
 
 include "../dbh.php";
 
